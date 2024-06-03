3f73fba5e1e18e9fc8846a578b470e72f26b7075
vebly <jh.haruna@gmail.com>
2024-06-03 17:49:45 +0200

    Improved wishlist list component

--- src/views/wishlist/ItemList.vue
+++ src/views/wishlist/ItemList.vue
@@ -1,42 +1,80 @@
 <template>
-    <v-data-table
-        :groupBy="groupBy"
-        :headers="headers"
-        :items="categories"
-        item-value="items"
-    >
-        <template v-slot:group-header="{item, columns, toggleGroup, isGroupOpen }">
-            <tr>
-                <td :colspan="columns.length">
-                    <VBtn
-                    :icon="isGroupOpen(item) ? '$expand' : '$next'"
-                    size="small"
-                    variant="text"
-                    @click="toggleGroup(item)"
+    <v-expansion-panels multiple>
+        <v-expansion-panel v-for="(category, index) in categories" :key="index">
+            <v-expansion-panel-title>
+                <div>{{category.name}}</div>
+                <v-btn 
+                    icon="mdi-plus" 
+                    density="compact" 
+                    @click.stop="addItem" 
                     >
-                    </VBtn>
-                    <div class="flex">
+                    <v-icon color="accent"></v-icon>
 
-                        <div>{{item.value}}</div>
-                        <div>
-                            <v-btn icon="mdi-plus" density="compact" @click="addItem" ><v-icon color="accent"></v-icon></v-btn>
-
-                        </div>
-
-                    </div>
-                </td>
-            </tr>
-        </template>
-
-        <template v-slot:item="{item, index}">
-            <tr v-for="(subItem, subIndex) in item.items" :key="subIndex">
-                <td> {{subItem.name}} </td>
-                <td> {{subItem.price}} </td>
-                <td> <v-btn icon="mdi-delete" density="compact" @click="removeItem(subIndex)"><v-icon color="red"></v-icon></v-btn></td>
-            </tr>
-        </template>
-    </v-data-table>
+                </v-btn>
+            </v-expansion-panel-title>
+            <v-expansion-panel-text>
+                <v-table>
+                    <tr v-for="(items, index) in category.items">
+                        <td> {{items.name}} </td>
+                        <td> {{items.price}} </td>
+                        <td> 
+                            <v-btn 
+                                icon="mdi-delete" 
+                                density="compact" 
+                                @click="removeItem(subIndex)"
+                                >
+                                <v-icon color="red"></v-icon>
+                            </v-btn>
+                        </td>
+                    </tr>
+                </v-table>
+            </v-expansion-panel-text>
+        </v-expansion-panel>
+    </v-expansion-panels>
 </template>
+<!-- <template> -->
+<!--     <v-data-table -->
+<!--         :groupBy="groupBy" -->
+<!--         :headers="headers" -->
+<!--         :items="categories" -->
+<!--         class="w-3" -->
+<!--         item-value="items" -->
+<!--     > -->
+<!--         <template v-slot:group-header="{item, columns, toggleGroup, isGroupOpen }"> -->
+<!--             <tr> -->
+<!--                 <td :colspan="columns.length"> -->
+<!--                     <div class="flex items-center"> -->
+<!--                         <VBtn -->
+<!--                         :icon="isGroupOpen(item) ? '$expand' : '$next'" -->
+<!--                         size="small" -->
+<!--                         variant="text" -->
+<!--                         @click="toggleGroup(item)" -->
+<!--                         > -->
+<!--                         </VBtn> -->
+<!---->
+<!--                         <div class="flex-grow">{{item.value}}</div> -->
+<!--                         <div> -->
+<!--                             <v-btn icon="mdi-plus" density="compact" @click="addItem" ><v-icon color="accent"></v-icon></v-btn> -->
+<!---->
+<!--                         </div> -->
+<!--                     </div> -->
+<!---->
+<!--                 </td> -->
+<!--             </tr> -->
+<!--         </template> -->
+<!---->
+<!--         <template v-slot:item="{item, index}"> -->
+<!--             <tr v-for="(subItem, subIndex) in item.items" :key="subIndex"> -->
+<!--                 <td class="flex-1"> {{subItem.name}} </td> -->
+<!--                 <td class="flex-1"> {{subItem.price}} </td> -->
+<!--                 <td class="flex-1">  -->
+<!--                     <v-btn icon="mdi-delete" density="compact" @click="removeItem(subIndex)"><v-icon color="red"></v-icon></v-btn> -->
+<!---->
+<!--                 </td> -->
+<!--             </tr> -->
+<!--         </template> -->
+<!--     </v-data-table> -->
+<!-- </template> -->
 
 <script setup>
     import {ref, reactive, computed} from 'vue';
